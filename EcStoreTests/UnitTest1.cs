fc8c4c6a99ad92980d74b60cb6ec9d268933bad3
Joey Chen <jay.hatelove@gmail.com>
2019-05-17 12:51:03 +0800

    init

---
+++ EcStoreTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_SyncBookOrders_3_Orders_Only_2_book_order()
+        {
+            // hard to isolate dependency to unit test
+
+            //var target = new OrderService();
+            //target.SyncBookOrders();
+            Assert.Fail();
+        }
+    }
+}
